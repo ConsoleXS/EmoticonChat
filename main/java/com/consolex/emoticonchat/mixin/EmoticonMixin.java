08b9e581b03e7c76b3b9db2ff8fbac54e8657d33
ConsoleXS <aditya.rao.2019.yeet@gmail.com>
2022-07-21 09:05:37 -0500

    Initial commit

---
+++ main/java/com/consolex/emoticonchat/mixin/EmoticonMixin.java
@@ -0,0 +1,68 @@
+package com.consolex.emoticonchat.mixin;
+
+import com.sun.jna.platform.win32.NTSecApi;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Mixin(ChatScreen.class)
+public class EmoticonMixin {
+	ClientPlayerEntity entity = MinecraftClient.getInstance().player;
+
+	@Shadow
+	protected TextFieldWidget chatField;
+
+	private final MinecraftClient client = MinecraftClient.getInstance();
+
+	@Inject(at = @At(value = "HEAD"), method = "keyPressed", cancellable = true)
+	private void addEmoji(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+		if (keyCode == GLFW.GLFW_KEY_ENTER || keyCode == GLFW.GLFW_KEY_KP_ENTER) {
+			String string = this.chatField.getText().trim();
+			HashMap<String, String> textToEmoji = new HashMap<>();
+			textToEmoji.put(":skull:", "☠");
+			textToEmoji.put(":v:", "✌");
+			textToEmoji.put(":heart:", "❤");
+			textToEmoji.put(":tm:", "™");
+			textToEmoji.put(":checkmark:", "✓");
+			textToEmoji.put(":star:", "★");
+			textToEmoji.put(":smile:", "☻");
+			textToEmoji.put(":infinity:", "∞");
+			textToEmoji.put(":e:", "Ⓔ");
+			textToEmoji.put(":x:", "✕");
+			textToEmoji.put(":tableflip:", "(╯°□°）╯︵ ┻━┻");
+			textToEmoji.put(":fire:", "🔥");
+
+
+
+
+			if (!string.isEmpty()) {
+				for (String key : textToEmoji.keySet())
+				{
+					if (string.contains(key))
+					{
+						string = string.replace(key, textToEmoji.get(key)).toString();
+						entity.sendChatMessage(string);
+						cir.cancel();
+					}
+				}
+
+
+			}
+			this.client.setScreen(null);
+
+		}
+	}
+
+
+}
